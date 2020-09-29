1bb0fce7cec3ccf638470058dfe714c4aa51d89f
Aditya Goyal <43295337+adityagoyal222@users.noreply.github.com>
2020-09-29 22:33:57 +0545

    implemented MVT paradigm

--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from AppTwo.models import User
 
 # Create your views here.
 def index(request):
@@ -8,3 +9,8 @@ def index(request):
 def help(request):
     my_dict = {'head_title': 'Help Page'}
     return render(request, 'AppTwo/help.html', context=my_dict)
+
+def users(request):
+    user_list = User.objects.order_by('first_name')
+    my_dict = {'user_list': user_list}
+    return render(request, 'AppTwo/user.html', context=my_dict)
