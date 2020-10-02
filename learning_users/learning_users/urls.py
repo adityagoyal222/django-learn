fb620bb8c039db60facea64c1d39824084aa1eae
Aditya Goyal <43295337+adityagoyal222@users.noreply.github.com>
2020-10-02 16:00:20 +0545

    Added templates and URLs

--- learning_users/learning_users/urls.py
+++ learning_users/learning_users/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from basic_app import views
 
 urlpatterns = [
+    url(r'^$', views.index, name="index"),
+    url(r'^basic_app/$', include('basic_app.urls')),
     path('admin/', admin.site.urls),
 ]
