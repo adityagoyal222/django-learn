728f10debdce3764ed4dec0cd2d05dfd6fb9e4ee
Aditya Goyal <43295337+adityagoyal222@users.noreply.github.com>
2020-10-03 16:28:16 +0545

    create a basic CBV using IndexView

--- advcbv/advcbv/urls.py
+++ advcbv/advcbv/urls.py
@@ -20,5 +20,5 @@ from basic_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', views.index, name="index")
+    url(r'^$', views.IndexView.as_view(), name="index")
 ]
