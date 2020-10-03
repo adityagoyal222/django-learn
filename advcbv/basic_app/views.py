f73c0e4c69f395e5d9b2a24743c65b6fc9061d5e
Aditya Goyal <43295337+adityagoyal222@users.noreply.github.com>
2020-10-03 17:01:29 +0545

    created list and detail views

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView, DetailView
+from basic_app import models
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
+class SchoolList(ListView):
+    model = models.School
+
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
